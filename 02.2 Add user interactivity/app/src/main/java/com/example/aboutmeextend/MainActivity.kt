379a5b347dc282f8d1b1e4ce9e80103d3ae4e36c
Radu Bogdan-Mircea <radu-bogdan@outlook.com>
2019-05-09 17:54:35 +0300

    02.2 event to editText so it acts like done_button. can update nickname
    test description?

--- 02.2 Add user interactivity/app/src/main/java/com/example/aboutmeextend/MainActivity.kt
+++ 02.2 Add user interactivity/app/src/main/java/com/example/aboutmeextend/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
@@ -25,6 +26,9 @@ class MainActivity : AppCompatActivity() {
     *   - display the nickname textView
     *   - hide the keyboard after the button has been pressed.
     *       by default it stays visible even after the button is pressed
+    *
+    *  create a click listener for the nickname_text so it shows the editText and button again
+    *
     * */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,8 +39,27 @@ class MainActivity : AppCompatActivity() {
         nicknameText = findViewById(R.id.nickname_text)
         doneButton = findViewById(R.id.done_button)
 
+        //an event listener so when the enter key on the keyboard is pressed,
+        //it acts like the done_button was clicked
+        editText.setOnKeyListener(object: View.OnKeyListener{
+            override fun onKey(v: View?, keyCode: Int, event: KeyEvent?): Boolean {
+                if(event?.action == KeyEvent.ACTION_DOWN){
+                    when(keyCode){
+                        KeyEvent.KEYCODE_ENTER -> {
+                            addNickname(this@MainActivity)
+                            return true
+                        }
+                    }
+                }
+                return false
+            }
+
+        })
+
         //setting the onclick listener
         doneButton.setOnClickListener { addNickname(this) }
+
+        nicknameText.setOnClickListener { updateNickname(it) }
     }
 
     /*
@@ -59,5 +82,21 @@ class MainActivity : AppCompatActivity() {
 
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(currentView.windowToken, 0)
+    }
+
+    //give a view as parameter so i know what im working with/which view calls this method
+    private fun updateNickname(view: View){
+        //make the button and editTextView visible again
+        editText.visibility = View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+        view.visibility = View.GONE
+
+        //make the ediText focused
+        editText.requestFocus()
+
+
+        //show the keyboard instantly, not after a second tap
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.showSoftInput(editText, 0)
     }
 }
