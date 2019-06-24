a207f2ab9ea87c76693161fc97816f079a8010a2
Radu Bogdan-Mircea <radu-bogdan@outlook.com>
2019-06-24 17:01:15 +0300

    ended up scrapping 03.1 because it had a bug and wasnt working. works rn

---
+++ 03.1 Create a fragment/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,52 @@
+package com.example.android.navigation
+
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        var binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
+                R.layout.fragment_title,
+                container,
+                false)
+
+        binding.playButton.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_titleFragment_to_gameFragment)
+        }
+
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, Navigation.findNavController(view!!))
+                || super.onOptionsItemSelected(item)
+    }
+
+
+}
--- 03.1 Create a fragment/readme.md
+++
@@ -1,4 +0,0 @@
-* I continued module 3.2 in this project because they are closely related
-* In 3.2 i'll be using the navigation graph. It is part of a library that helps define a specific path for your fragments.
-* Every fragment in the app should be part of something called **NavHostFragment**
-* In **activity_main** ill use **NavHostFragment** instead of the title one
