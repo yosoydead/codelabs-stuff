9005f95238a18de35c564fe0424a0e85322998f7
Radu Bogdan-Mircea <radu-bogdan@outlook.com>
2019-07-04 18:26:16 +0300

    06.1 created the SleeperDatabase db holder

--- 06.1 creating a room database/app/src/main/java/com/example/trackmysleep/database/SleepDatabase.kt
+++ 06.1 creating a room database/app/src/main/java/com/example/trackmysleep/database/SleepDatabase.kt
@@ -1,2 +1,55 @@
 package com.example.trackmysleep.database
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+//the database annotation needs 3 parameters: list of entity/entities, ersion number, bool to export schema
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase: RoomDatabase(){
+    abstract val sleepDatabaseDao: SleepDatabaseDao
+
+    /*
+    * this object allows clients to access methods for creating or getting the database
+    * without instantiating the class
+    * */
+    companion object {
+
+        /*
+        * volatile means the variable will never be cached and all writes and reads will be done
+        *   to and from the main memory
+        *  this helps make sure the value of INSTANCE is always up-to-date and the same to all
+        *   execution threads. changes made by one thread to INSTANCE are visible to all other threads
+        *   immediately and you dont get a situation where two threads each update the same entity in a cache
+        * */
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
+
+        //return the instance for the database
+        fun getInstance(context: Context): SleepDatabase{
+            /*
+            * by wrapping the code in this block means that only one thread of execution at a time
+            *   can enter this block of code, which makes sure the database only gets initialized once
+            *
+            * multiple threads can ask for a database instance at the same time. this eliminates the problem
+            * */
+            synchronized(this){
+                var instance = INSTANCE
+
+                //if the databse instance is null(doesnt exist) use the databaseBuilder to make one
+                if(instance == null){
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        SleepDatabase::class.java,
+                        "sleep_history_database")
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+
+                return instance
+            }
+        }
+    }
+}
--- 06.1 creating a room database/readme.md
+++ 06.1 creating a room database/readme.md
@@ -1,3 +1,4 @@
+* This code will be for 06.1 and 06.2 modules
 * In Android, data is represented in data classes  (data is modified/accessed through function calls)
 * In a database, data represents an **entity** and it is accessed through **queries**
     - an **entity** represents an object and its properties to store in the database. An entity class defines a table, each instance of that class represents a row in the table. Each property defines a column
@@ -5,3 +6,4 @@
 * **Room** is a framework that does all the hard work of getting from data classes to entities that can be stored in **SQLite** and from function declarations to **SQL queries**
 * Each **entity** must be defined as an annotated *data class* and the **interactions** as an annotated *interface*. **Room** uses these annotated classes to create tables in the database and queries that act on the database.
 * **DAO** provides convenience methods for inserting, deleting and updating the database. It is defined as an *interface* using annotations
+* To create a database from the **entity** and the **dao**, an abstract class annotated **@Database** needs to be created. This class has one method that either creates an instance of the database if it doesn't exist or returns a reference to an existing database. This class needs to inherit from **RoomDatabase** and will act as a database holder. The app needs only **one** instance of the database for the whole app.
